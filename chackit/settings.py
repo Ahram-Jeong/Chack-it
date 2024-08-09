ba2c0c7b0391da28aa72fd367a6f9b8cb55c95a1
Ahram-Jeong <ahram1215@gmail.com>
2024-08-09 13:29:23 +0900

    20240809 - 기본 셋팅, users 앱 추가 등

--- chackit/settings.py
+++ chackit/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # app 추가
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
