01359a94e12ad8f2ab9c78fbf7a726eec6826ce9
Ahram-Jeong <ahram1215@gmail.com>
2024-08-09 21:03:05 +0900

    20240809 - filemanager-webpack-plugin 추가

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+
 module.exports = defineConfig({
   transpileDependencies: true,
-    // 빌드 결과물 경로 추가
+  // 빌드 결과물 경로 추가
   outputDir: 'dist', // 빌드 시 생성 된 파일들이 저장될 디렉토리
   publicPath: '/', // 배포 될 기본 URL 경로
   assetsDir: 'static', // 빌드 된 static 파일들이 저장 될 하위 디렉토리
@@ -10,5 +12,30 @@ module.exports = defineConfig({
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
-  }
+  },
+  
+  // filemanager-webpack-plugin 설정 추가
+  configureWebpack: {
+    plugins: [
+      new FileManagerPlugin({
+        events: {
+          onStart: {
+            delete: [
+              '../static/**/',
+              '../templates/**/',
+            ],
+          },
+
+          onEnd: {
+            copy: [
+              { source: 'dist/static', destination: '../static/' },
+              { source: 'dist/favicon.ico', destination: '../static/img/' },
+              { source: 'dist/index.html', destination: '../templates/' },
+            ],
+          },
+        },
+      }),
+    ]
+  },
+
 })
