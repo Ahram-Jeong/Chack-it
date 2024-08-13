d055d32e8b611b01a0ea1867e5967aa94f7ca4d6
Ahram-Jeong <ahram1215@gmail.com>
2024-08-14 00:12:35 +0900

    20240814 - settings STATICFILES_DIRS 경로 수정 등

--- chackit/settings/base.py
+++ chackit/settings/base.py
@@ -75,7 +75,7 @@ ROOT_URLCONF = 'chackit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/dist')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -138,7 +138,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/dist/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+
   // 빌드 결과물 경로 추가
   outputDir: 'dist', // 빌드 시 생성 된 파일들이 저장될 디렉토리
   publicPath: '/', // 배포 될 기본 URL 경로
@@ -13,29 +13,4 @@ module.exports = defineConfig({
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
   },
-  
-  // filemanager-webpack-plugin 설정 추가
-  configureWebpack: {
-    plugins: [
-      new FileManagerPlugin({
-        events: {
-          onStart: {
-            delete: [
-              '../static/**/',
-              '../templates/**/',
-            ],
-          },
-
-          onEnd: {
-            copy: [
-              { source: 'dist/static', destination: '../static/' },
-              { source: 'dist/favicon.ico', destination: '../static/img/' },
-              { source: 'dist/index.html', destination: '../templates/' },
-            ],
-          },
-        },
-      }),
-    ]
-  },
-
 })
